85520d7eb99cd158cc10710dbf849aa7206db179
FranGz8 <0javalife@gmail.com>
2021-10-07 22:37:03 -0600

    :sparkles: Peliculas y series añadidas

--- src/screens/Peliculas.js
+++ src/screens/Peliculas.js
@@ -1,20 +1,116 @@
 import React from 'react';
-import {View,Text,Image,StyleSheet} from 'react-native';
+import {View,Text,Image,StyleSheet, ScrollView, TouchableHighlight, Linking, Alert} from 'react-native';
+
 export default function Peliculas(){
+
  return(
  <View>
-        <View>
-            <Text style={styles.titulo}>Estamos en Peliculas</Text>
+        <ScrollView>
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Comedia</Text>
+          <ScrollView horizontal>
+            <View>
+              <TouchableHighlight onPress={()=>{openModal1()}}>
+              <Image style={styles.pelicula} source={require('../img/comedia1.jpg')} />
+              </TouchableHighlight>  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/comedia2.jpg')} />  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/comedia3.jpg')} />  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/comedia4.jpg')} />  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/comedia5.jpg')} />  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/comedia6.jpg')} />  
+            </View>
+          </ScrollView>
         </View>
+        </ScrollView>
+
+        <ScrollView>
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Romance</Text>
+          <ScrollView horizontal>
+            <View>
+              <TouchableHighlight onPress={()=>{openModal1()}}>
+              <Image style={styles.pelicula} source={require('../img/romance1.jpg')} />
+              </TouchableHighlight>  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/romance2.jpg')} />  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/romance3.jpg')} />  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/romance4.jpg')} />  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/romance5.jpg')} />  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/romance6.jpg')} />  
+            </View>
+          </ScrollView>
+        </View>
+        </ScrollView>
+
+        <ScrollView>
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Acción</Text>
+          <ScrollView horizontal>
+            <View>
+              <TouchableHighlight onPress={() => Linking.openURL('https://youtube.com/watch?v=_CazeprALO8')}>
+              <Image style={styles.pelicula} source={require('../img/accion1.jpg')} />
+              </TouchableHighlight>  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/accion2.jpg')} />  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/accion3.jpg')} />  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/accion4.jpg')} />  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/accion5.jpg')} />  
+            </View>
+            <View>
+              <Image style={styles.pelicula} source={require('../img/accion6.jpg')} />  
+            </View>
+          </ScrollView>
+        </View>
+        </ScrollView>
+
  </View>
  );
 }
 
 const styles = StyleSheet.create({ 
+    banner:{ 
+      height:250, 
+      flex:1 
+    },
     
     titulo:{
       fontWeight:'bold',
       fontSize:24,
+      marginVertical:10
     },
     
-}); 
+    contenedor:{
+      marginHorizontal:10
+    },
+    
+    pelicula:{
+      marginRight:10
+    },
+
+    }); 
--- src/screens/Series.js
+++ src/screens/Series.js
@@ -1,20 +1,190 @@
 import React from 'react';
-import {View,Text,Image,StyleSheet} from 'react-native';
+import {View,Text,Image,StyleSheet, ScrollView, TouchableHighlight, Linking} from 'react-native';
 export default function Series(){
 return(
-<View>
-<View>
-<Text style={styles.titulo}>Estamos en Series</Text>
-</View>
+    <View>
+    <ScrollView>
+    <View style={styles.listado}>
+    <Text style={styles.titulo}>Comedia</Text>
+      <View style={styles.listaItem}>
+        <TouchableHighlight onPress={() => Linking.openURL('https://youtube.com/watch?v=_CazeprALO8')}>
+        <Image  style={styles.foto} source={require('../img/serieComedia1.jpg')} />
+        </TouchableHighlight>
+        <View style={styles.data}>
+            <Text style={styles.nombre}>The Big Bang Theory</Text>
+            <Text style={styles.descripcion}>Temporadas: 12</Text>
+        </View>
+      </View>
+
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieComedia2.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>Rick y Morty</Text>
+            <Text style={styles.descripcion}>Temporadas: 5</Text>
+        </View>
+      </View>
+
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieComedia3.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>Modern Family</Text>
+            <Text style={styles.descripcion}>Temporadas: 11</Text>
+        </View>
+      </View>
+
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieComedia4.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>Dos hombres y medio</Text>
+            <Text style={styles.descripcion}>Temporadas: 12</Text>
+        </View>
+      </View>
+
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieComedia5.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>Los Simpson</Text>
+            <Text style={styles.descripcion}>Temporadas: 33</Text>
+        </View>
+      </View>
+    </View>
+
+
+    <View style={styles.listado}>
+    <Text style={styles.titulo}>Aventura</Text>
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieAventura1.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>The Mandalorian</Text>
+            <Text style={styles.descripcion}>Temporadas: 2</Text>
+        </View>
+      </View>
+
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieAventura2.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>Flecha</Text>
+            <Text style={styles.descripcion}>Temporadas: 8</Text>
+        </View>
+      </View>
+
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieAventura3.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>Vikingos</Text>
+            <Text style={styles.descripcion}>Temporadas: 5</Text>
+        </View>
+      </View>
+
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieAventura4.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>Star Wars: La guerra de los clones</Text>
+            <Text style={styles.descripcion}>Temporadas: 7</Text>
+        </View>
+      </View>
+
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieAventura5.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>Loki</Text>
+            <Text style={styles.descripcion}>Temporadas: 1</Text>
+        </View>
+      </View>
+    </View>
+
+
+    <View style={styles.listado}>
+    <Text style={styles.titulo}>Fantasia</Text>
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieFantasia1.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>Merlín</Text>
+            <Text style={styles.descripcion}>Temporadas: 5</Text>
+        </View>
+      </View>
+
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieFantasia2.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>The Flash</Text>
+            <Text style={styles.descripcion}>Temporadas: 8</Text>
+        </View>
+      </View>
+
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieFantasia3.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>Supergirl</Text>
+            <Text style={styles.descripcion}>Temporadas: 6</Text>
+        </View>
+      </View>
+
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieFantasia4.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>DC: Leyendas del mañana</Text>
+            <Text style={styles.descripcion}>Temporadas: 6</Text>
+        </View>
+      </View>
+
+      <View style={styles.listaItem}>
+        <Image  style={styles.foto} source={require('../img/serieFantasia5.jpg')} />
+        <View style={styles.data}>
+            <Text style={styles.nombre}>Titanes</Text>
+            <Text style={styles.descripcion}>Temporadas: 1</Text>
+        </View>
+      </View>
+    </View>    
+
+    </ScrollView>
 </View>
 );
 }
 
 const styles = StyleSheet.create({ 
+    
+    listado:{
 
-titulo:{
-fontWeight:'bold',
-fontSize:24,
-},
+    },
 
-}); 
+    titulo:{
+      fontWeight:'bold',
+      fontSize:24,
+      marginVertical:10
+    },
+    
+    
+    foto:{
+      marginVertical:5,
+      left:40,
+      width:122,
+      height:162,
+    },
+    
+    listaItem:{
+      flexBasis:'20%',
+      backgroundColor: '#EEEEEE',
+      flexDirection:'row',
+
+    },
+
+    data:{
+
+      },
+    
+    nombre:{
+        fontWeight:'bold',
+        fontSize:22,
+        color:'rgb(0, 122, 255)',
+        left:50
+    },
+
+    descripcion:{
+        fontSize:18,
+        color:'#000',
+        left:50,
+        marginVertical:45
+    },
+    
+    }); 
